f962af58966e5c461016e10f661c3742323e8c1e
AKCarrasco <akcarrasco16@gmail.com>
2026-02-05 11:22:39 -0500

    Assignment 2 finish with error log

--- assignment2.py
+++ assignment2.py
@@ -1,21 +1,60 @@
 import argparse
+import csv
 import urllib.request
 import logging
 import datetime
 
 def downloadData(url):
     """Downloads the data"""
-    pass
+    with urllib.request.urlopen(url) as response:
+        return response.read().decode("utf-8")
 
 def processData(file_content):
-    pass
+    data_dict= {}
+    logger= logging.getLogger('assignment2')
+    csv_reader = csv.reader(file_content.splitlines())
 
+    for linenum, row in enumerate(csv_reader, start=1):
+        person_id, name, birthday = row
+        try:
+            birthday = datetime.datetime.strptime(birthday, '%m/%d/%Y').date()
+            data_dict[int(person_id)] = (name, birthday)
+        except ValueError:
+            logger.error(f"Error reading line #{linenum} for ID #{person_id}")
+
+    return data_dict
 
 def displayPerson(id, personData):
-    pass
+    """Display the person data"""
+    if id in personData:
+        name, birthday = personData[id]
+        print(f"Person #{id}: is {name} with a birthday of {birthday}")
+    else:
+        print("ID not found")
+
+def setuplogger():
+    logging.basicConfig(filename='error.log', level=logging.ERROR)
 
 def main(url):
     print(f"Running main with URL = {url}...")
+    setuplogger()
+
+    try:
+        csv_data = downloadData(url)
+    except urllib.error.URLError as e:
+        print(f"Error downloading data from {url}: {e.reason}")
+        return
+
+    person_data = processData(csv_data)
+
+    while True:
+        try:
+            user_input = int(input("Enter a person ID to display (0 to quit): "))
+            if user_input == 0:
+                break
+            displayPerson(user_input, person_data)
+        except ValueError:
+            print("Please enter a valid ID")
 
 
 if __name__ == "__main__":
